7a96deed17f32bf65c53f36ca5ca18ed37878518
Kai Waldrant <kai@data-intuitive.com>
2024-06-09 22:48:00 +0200

    relocate scripts

---
+++ scripts/add_a_method.sh
@@ -0,0 +1,6 @@
+component_name="my_method"
+component_lang="python" # change this to "r" if need be
+
+common/create_component/create_component \
+  --language "$component_lang" \
+  --name "$component_name"
