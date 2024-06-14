8c0f89c4ce8de03e3c3a4cf4060d814cb8f935e4
Kai Waldrant <kai@data-intuitive.com>
2024-06-14 23:14:24 +0200

    WIP instructions

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -5,6 +5,6 @@ set -e
 echo ">> Downloading resources"
 
 viash run common/src/sync_resources/config.vsh.yaml -- \
-  --input "s3://openproblems-bio/public/neurips-2023-competition/workflow-resources/" \
+  --input "s3://openproblems-data/resources_test/common/" \
   --output "resources" \
   --delete
