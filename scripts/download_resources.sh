e649c2909f3d9852d728f6c02f3b0d051e025998
Kai Waldrant <kai@data-intuitive.com>
2024-06-21 15:30:57 +0200

    Fix resources

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -4,7 +4,13 @@ set -e
 
 echo ">> Downloading resources"
 
-viash run common/src/sync_resources/config.vsh.yaml -- \
+common/sync_resources/sync_resources \
   --input "s3://openproblems-data/resources_test/common/" \
-  --output "resources_test" \
+  --output "resources_test/common" \
   --delete
+
+# After finishing the task and the task specific test_resources are uploaded to s3, uncomment:
+# common/sync_resources/sync_resources \
+#   --input "s3://openproblems-data/resources_test/<task_name>/" \
+#   --output "resources_test/<task_name>" \
+#   --delete
