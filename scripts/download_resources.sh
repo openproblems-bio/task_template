aafaa760422906946444a5a0c56d10cfa1e5f925
Kai Waldrant <kai@data-intuitive.com>
2024-07-10 16:29:35 +0200

    Update download resources

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -5,8 +5,6 @@ set -e
 echo ">> Downloading resources"
 
 common/sync_resources/sync_resources \
-  --input "s3://openproblems-data/resources_test/common/" \
-  --output "resources_test/common" \
   --delete
 
 # After finishing the task and the task specific test_resources are uploaded to s3, uncomment:
