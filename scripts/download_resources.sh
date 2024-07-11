2909f3489da790567d9f1e0ff28c8c052154a11b
Kai Waldrant <kai@data-intuitive.com>
2024-07-11 21:12:46 +0200

    Add comment to download resources

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -4,5 +4,6 @@ set -e
 
 echo ">> Downloading resources"
 
+# the sync_resources script uses the test_resources S3 URI's in the _viash.yaml to download the resources.
 common/sync_resources/sync_resources \
   --delete
