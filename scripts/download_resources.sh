18a58299999ed4ea22c535cd9e54c1970e29b3f7
Kai Waldrant <kai@data-intuitive.com>
2024-07-11 21:06:01 +0200

    remove comment from download_resources script

--- scripts/download_resources.sh
+++ scripts/download_resources.sh
@@ -6,9 +6,3 @@ echo ">> Downloading resources"
 
 common/sync_resources/sync_resources \
   --delete
-
-# After finishing the task and the task specific test_resources are uploaded to s3, uncomment:
-# common/sync_resources/sync_resources \
-#   --input "s3://openproblems-data/resources_test/<task_name>/" \
-#   --output "resources_test/<task_name>" \
-#   --delete
