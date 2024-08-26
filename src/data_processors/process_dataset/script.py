38a37a82bd8f9ebc68005d05732dcf218c6d1acd
rcannood <rcannood@users.noreply.github.com>
2024-08-26 12:44:48 +0000

    deploy: a3e0dae86d6bb6fd7d4d6b99d9cc74accfaf3405

--- src/data_processors/process_dataset/script.py
+++ src/data_processors/process_dataset/script.py
@@ -2,6 +2,7 @@ import sys
 import random
 import numpy as np
 import anndata as ad
+import openproblems as op
 
 ## VIASH START
 par = {
@@ -15,14 +16,16 @@ par = {
     'output_solution': 'solution.h5ad'
 }
 meta = {
-    'resources_dir': 'data_processors/process_dataset',
-    'config': 'data_processors/process_dataset/.config.vsh.yaml'
+    'resources_dir': 'target/executable/data_processors/process_dataset',
+    'config': 'target/executable/data_processors/process_dataset/.config.vsh.yaml'
 }
 ## VIASH END
 
 # import helper functions
 sys.path.append(meta['resources_dir'])
-from subset_anndata import read_config_slots_info, subset_anndata
+from subset_h5ad_by_format import subset_h5ad_by_format
+
+config = op.project.read_viash_config(meta["config"])
 
 # set seed if need be
 if par["seed"]:
@@ -52,24 +55,29 @@ slot_mapping = {
         "batch": par["obs_batch"],
     }
 }
-slot_info = read_config_slots_info(meta["config"], slot_mapping)
 
 print(">> Creating train data", flush=True)
-output_train = subset_anndata(
-    adata[[not x for x in is_test]], 
-    slot_info["output_train"]
+output_train = subset_h5ad_by_format(
+    adata[[not x for x in is_test]],
+    config,
+    "output_train",
+    slot_mapping
 )
 
 print(">> Creating test data", flush=True)
-output_test = subset_anndata(
+output_test = subset_h5ad_by_format(
     adata[is_test],
-    slot_info["output_test"]
+    config,
+    "output_test",
+    slot_mapping
 )
 
 print(">> Creating solution data", flush=True)
-output_solution = subset_anndata(
+output_solution = subset_h5ad_by_format(
     adata[is_test],
-    slot_info['output_solution']
+    config,
+    "output_solution",
+    slot_mapping
 )
 
 print(">> Writing data", flush=True)
