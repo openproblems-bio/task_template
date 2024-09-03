22def3f66f9a45e2c1f7733649e685f49ce781a4
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-03 04:24:29 +0200

    update scripts

--- scripts/download_resources.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo ">> Downloading resources"
-
-# the sync_resources script uses the test_resources S3 URI's in the _viash.yaml to download the resources.
-common/scripts/sync_resources \
-  --delete
--- scripts/run_benchmark/run.sh
+++ scripts/run_benchmark/run.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# get the root of the directory
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# ensure that the command below is run from the root of the repository
+cd "$REPO_ROOT"
+
+# remove this when you have implemented the script
+echo "TODO: once the 'run_benchmark' workflow has been implemented, update this script to use it."
+echo "  Step 1: replace 'task_template' with the name of the task in the following command."
+echo "  Step 2: replace the rename keys parameters to fit your run_benchmark inputs"
+echo "  Step 3: replace the settings parameter to fit your run_benchmark outputs"
+echo "  Step 4: remove this message"
+exit 1
+
+set -e
+
+# generate a unique id
 RUN_ID="run_$(date +%Y-%m-%d_%H-%M-%S)"
 publish_dir="s3://openproblems-data/resources/task_template/results/${RUN_ID}"
 
